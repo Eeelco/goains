f786e8235ff65ee209f366680abdaf4fb994306b
Eelco Empting <me@eelco.de>
2024-03-21 20:12:19 +0100

    Remove unneeded files

--- app.go
+++ app.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"os"
 	"strings"
 
 	"github.com/wailsapp/wails/v2/pkg/runtime"
@@ -33,48 +31,10 @@ func (a *App) startup(ctx context.Context) {
 		}
 	}
 	initializeConfigVariables()
-	CheckConfigFolder()
-	LoadConfigAndDB()
-
-}
-
-// Check if the config folder exists, if not, create it
-func CheckConfigFolder() bool {
 	if !configFolderExists() {
 		createConfigFolder()
-		return false
 	}
-	return true
-}
-
-// Conditionally download exercise database
-func (a *App) DownloadDatabase() {
-	askDownloadOptions := runtime.MessageDialogOptions{Type: runtime.QuestionDialog, Title: "Download Database?", Message: "No exercise database found. Would you like to download it now (approx. 100 MB)?"}
-	response, err := runtime.MessageDialog(a.ctx, askDownloadOptions)
-
-	if err != nil || response != "Yes" {
-		return
-	}
-	runtime.EventsEmit(a.ctx, "download_db")
-	DownloadFile(exercises_json, DB_FILE)
-
-	exercises, _ := os.Open(DB_FILE)
-	defer exercises.Close()
-
-	dec := json.NewDecoder(exercises)
-	_ = dec.Decode(&exerciseDatabase)
-
-	os.Mkdir(CONFIG_DIR+"/img", 0755)
-
-	for i, ex := range exerciseDatabase {
-		runtime.EventsEmit(a.ctx, "download_db_progress", [2]int{i, len(exerciseDatabase)})
-		os.Mkdir(CONFIG_DIR+"/img/"+ex.Id, 0755)
-		for _, img := range ex.Images {
-			DownloadFile(image_url_prefix+img, CONFIG_DIR+"/img/"+img)
-		}
-
-	}
-	runtime.EventsEmit(a.ctx, "download_db_done")
+	LoadConfigAndDB()
 
 }
 
