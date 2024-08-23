13d433b709822ac9e05552c0855de4d25d0c39b0
Eelco Empting <me@eelco.de>
2024-08-23 17:05:10 +0200

    Embed images in binary

--- main.go
+++ main.go
@@ -2,10 +2,6 @@ package main
 
 import (
 	"embed"
-	"fmt"
-	"net/http"
-	"os"
-	"strings"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -14,27 +10,6 @@ import (
 
 //go:embed all:frontend/build
 var assets embed.FS
-
-type FileLoader struct {
-	http.Handler
-}
-
-func NewFileLoader() *FileLoader {
-	return &FileLoader{}
-}
-
-func (h *FileLoader) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	var err error
-	requestedFilename := strings.TrimPrefix(req.URL.Path, "/")
-	println("Requesting file:", requestedFilename)
-	fileData, err := os.ReadFile(requestedFilename)
-	if err != nil {
-		res.WriteHeader(http.StatusBadRequest)
-		res.Write([]byte(fmt.Sprintf("Could not load file %s", requestedFilename)))
-	}
-
-	res.Write(fileData)
-}
 
 func main() {
 	// Create an instance of the app structure
@@ -46,8 +21,7 @@ func main() {
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
-			Assets:  assets,
-			Handler: NewFileLoader(),
+			Assets: assets,
 		},
 		BackgroundColour: &options.RGBA{R: 238, G: 238, B: 238, A: 210},
 		OnStartup:        app.startup,
