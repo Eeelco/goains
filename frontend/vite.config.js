d4f79f9bcdcae072fe399b46b04965a890c39636
Eelco Empting <me@eelco.de>
2024-08-29 21:34:45 +0200

    Remove unused dependency

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
-import { enhancedImages } from "@sveltejs/enhanced-img";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [enhancedImages(), sveltekit()],
+  plugins: [sveltekit()],
 });
